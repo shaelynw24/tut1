d8457fc4599165a7c67edb0b1d0f3ee7aab8b1dc
shaelynw24 <shaelyn.whitehead@gmail.com>
2024-07-25 13:17:42 +1200

    First commit

---
+++ Greeter.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Greeter {
+
+  private Locale locale;
+  private String name;
+
+  public Greeter(String language, String country, String name) {
+    locale = new Locale(language, country);
+    this.name = name;
+  }
+
+  public String sayHello() {
+    ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+<<<<<<< HEAD
+    return messages.getString("greeting") + ", " + name;
+=======
+    return messages.getString("greetings!") + ", " + name.toUpperCase();
+>>>>>>> feature-1
+  }
+
+  public static void main(String[] args) {
+    String language = args[0];
+    String country = args[1];
+    String name = args[2];
+    Greeter greeter = new Greeter(language, country, name);
+    System.out.println(greeter.sayHello());
+  }
+}
